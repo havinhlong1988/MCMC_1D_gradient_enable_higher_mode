a29f05e40d40aa1f9d745475d91e1f3f1ab399be
Ha Vinh Long <havinhlong@gmail.com>
2026-08-17 17:45:39 +0700

    higher mode run good - plot have an issue

--- CHT/MonteCarlo/01_run_MCMC.sh
+++ CHT/MonteCarlo/01_run_MCMC.sh
@@ -1,2 +1,2 @@
-sh make_file_control.sh 00740 
-time /home/vinh-longha/Research/MCMC_CHT/20260801_MCMC_CHT_monotonic_vpvs_H14/Codes/MCMC_flex/do_MC_Para /home/vinh-longha/Research/MCMC_CHT/20260801_MCMC_CHT_monotonic_vpvs_H14/CHT/data/00740_data/00740.control
+sh make_file_control.sh 00990 
+time /home/vinh-longha/Research/MCMC_CHT/20260801_MCMC_CHT_monotonic_vpvs_H14/Codes/MCMC_flex/do_MC_Para /home/vinh-longha/Research/MCMC_CHT/20260801_MCMC_CHT_monotonic_vpvs_H14/CHT/data/00990_data/00990.control
